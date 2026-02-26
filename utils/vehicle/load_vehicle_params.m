509616a696be8296b671c19814619084d6f9ac66
Vitor Machado de Toledo <119512960+vitormtt@users.noreply.github.com>
2026-02-26 15:45:09 -0300

    fix: replace heavy truck parameters with the original Blazer/SUV functional parameters

--- utils/vehicle/load_vehicle_params.m
+++ utils/vehicle/load_vehicle_params.m
@@ -11,59 +11,63 @@ function vehicle = load_vehicle_params(type)
 %   vehicle - Struct com 40 parâmetros físicos
 %
 % Referência:
-%   Gaspar et al. (2004) / Vu et al. (2016)
-%   Single Unit Heavy Vehicle (Caminhão Pesado)
+%   Código base anterior - Blazer/Caminhonete
 %
 % Vitor Yukio - UnB/PIBIC - 11/02/2026
 
     switch type
         case 'truck'
             %% Massa e Inércia
-            vehicle.m = 14193;      % Massa total (kg)
-            vehicle.ms = 12487;     % Massa suspensa (kg)
-            vehicle.muf = 706;      % Massa não suspensa frontal (kg)
-            vehicle.mur = 1000;     % Massa não suspensa traseira (kg)
-            vehicle.Ixx = 24201;    % Momento de inércia de rolagem (kg·m²)
-            vehicle.Izz = 34917;    % Momento de inércia de guinada (kg·m²)
-            vehicle.Ixz = 4200;     % Produto de inércia yaw-roll (kg·m²)
+            vehicle.ms = 1760; % Massa suspensa (kg)
+            vehicle.muf = 61; % Massa não suspensa no eixo dianteiro (kg)
+            vehicle.mur = 53; % Massa não suspensa no eixo traseiro (kg)
+            vehicle.m = vehicle.ms + vehicle.muf + vehicle.mur; % Massa total (kg)
+            
+            vehicle.Ixx = 527.927421; % Momento de inércia de rolagem (kg·m²)
+            vehicle.Ixz = 0.059089; % Produto de inércia yaw-roll (kg·m²)
+            vehicle.Izz = 2943.60885; % Momento de inércia de guinada (kg·m²)
             
             %% Geometria do Veículo
-            vehicle.h = 0.83;       % Altura CG massa suspensa (m) [hr na vdd, ajuste feito abaixo]
-            vehicle.hu = 0.53;      % Altura CG massa não suspensa (m)
-            vehicle.rf = 1.15;      % Altura eixo de rolagem frontal (m) [h na vdd]
-            vehicle.rr = 1.15;      % Altura eixo de rolagem traseiro (m)
-            vehicle.lf = 1.95;      % Distância CG → eixo dianteiro (m)
-            vehicle.lr = 1.54;      % Distância CG → eixo traseiro (m)
-            vehicle.lw = 0.93;      % Metade da largura do veículo (m)
-            vehicle.w = vehicle.lf + vehicle.lr; % Wheelbase (m)
+            vehicle.h = 0.626; % Altura CG massa suspensa (m)
+            vehicle.hu = 0.30; % Altura CG massa não suspensa (m)
+            
+            vehicle.rf = 0.07; % Altura eixo de rolagem frontal (m)
+            vehicle.rr = 0.19; % Altura eixo de rolagem traseiro (m)
+            
+            vehicle.lf = 1.332; % Distância CG → eixo dianteiro (m)
+            vehicle.w = 2.874; % Wheelbase (m)
+            vehicle.lr = vehicle.w - vehicle.lf; % Distância CG → eixo traseiro (m)
+            
+            vehicle.lw = 2.874 / 2; % Referência mantida pra retrocompatibilidade
             
             %% Suspensão
-            vehicle.kf = 380000;    % Rigidez rolagem suspensão frontal (Nm/rad)
-            vehicle.kr = 684000;    % Rigidez rolagem suspensão traseira (Nm/rad)
-            vehicle.bf = 100000;    % Amortecimento rolagem frontal (Nms/rad)
-            vehicle.br = 100000;    % Amortecimento rolagem traseiro (Nms/rad)
+            vehicle.kf = 95760.15; % Rigidez rolagem suspensão frontal (Nm/rad)
+            vehicle.kr = 53857.62; % Rigidez rolagem suspensão traseira (Nm/rad)
+            vehicle.bf = 6121.10; % Amortecimento rolagem frontal (Nms/rad)
+            vehicle.br = 3162.43; % Amortecimento rolagem traseiro (Nms/rad)
             
             %% Pneus
-            vehicle.ktf = 2060000;  % Rigidez rolagem pneu frontal (Nm/rad)
-            vehicle.ktr = 3337000;  % Rigidez rolagem pneu traseiro (Nm/rad)
-            vehicle.Cf = 582000;    % Cornering stiffness frontal (N/rad)
-            vehicle.Cr = 783000;    % Cornering stiffness traseira (N/rad)
+            vehicle.ktf = 258000; % Rigidez rolagem pneu frontal (Nm/rad)
+            vehicle.ktr = 229000; % Rigidez rolagem pneu traseiro (Nm/rad)
+            vehicle.Cf = 68800; % Cornering stiffness frontal (N/rad)
+            vehicle.Cr = 103200; % Cornering stiffness traseira (N/rad)
             
             %% Barra Estabilizadora (ARB) - Geometria
-            vehicle.tAf = 2.4076;   % Braço principal frontal (m)
-            vehicle.tBf = 0.6593;   % Braço secundário frontal (m)
-            vehicle.cf = 0.8931;    % Fator geométrico frontal (m)
-            vehicle.tAr = 2.8629;   % Braço principal traseiro (m)
-            vehicle.tBr = 0.4113;   % Braço secundário traseiro (m)
-            vehicle.cr = 0.7651;    % Fator geométrico traseiro (m)
+            vehicle.tAf = 0.766; % Braço principal frontal (m)
+            vehicle.tBf = 0.550; % Braço secundário frontal (m)
+            vehicle.cf = 0.285; % Fator geométrico frontal (m)
+            
+            vehicle.tAr = 0.732; % Braço principal traseiro (m)
+            vehicle.tBr = 0.514; % Braço secundário traseiro (m)
+            vehicle.cr = 0.285; % Fator geométrico traseiro (m)
             
             %% ARB - Rigidez Torcional
-            vehicle.kAO_f = 10730;  % Rigidez ARB frontal (Nm/rad)
-            vehicle.kAO_r = 15480;  % Rigidez ARB traseira (Nm/rad)
+            vehicle.kAO_f = 2500; % Rigidez ARB frontal (Nm/rad)
+            vehicle.kAO_r = 1500; % Rigidez ARB traseira (Nm/rad)
             
             %% ARB - Dimensional (informativo)
-            vehicle.Df = 32;        % Diâmetro externo frontal (mm)
-            vehicle.Dr = 34;        % Diâmetro externo traseiro (mm)
+            vehicle.Df = 2.253e-02; % Diâmetro externo frontal (mm)
+            vehicle.Dr = 2.000e-02; % Diâmetro externo traseiro (mm)
             
             %% Parâmetros Ambientais
             vehicle.mu = 1.0;       % Coeficiente de aderência (asfalto seco)
@@ -76,13 +80,13 @@ function vehicle = load_vehicle_params(type)
             vehicle.SSF = (vehicle.lw * 2) / (2 * vehicle.h); % Static Stability Factor
             
             %% Metadata
-            vehicle.name = 'Single Unit Heavy Vehicle (Gaspar 2004 / Vu 2016)';
-            vehicle.reference = 'Gaspar et al. (2004), Vu et al. (2016)';
+            vehicle.name = 'Chevrolet Blazer 2001';
+            vehicle.reference = 'Código Base Funcional Anterior';
             vehicle.dof = 14; 
             
         otherwise
             error('VehicleParams:InvalidType', ...
-                'Tipo de veículo inválido: "%s". Use ''truck''.', type);
+                'Tipo de veículo inválido: \"%s\". Use ''truck''.', type);
     end
     
     %% Validação de Consistência Física
